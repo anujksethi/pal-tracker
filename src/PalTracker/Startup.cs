d1c5aed4310ce6ddd264c4da6da2f8a63bd028e0
PAL-5 <anujksethi@gmail.com>
2019-08-28 11:16:31 +0530

    health

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,7 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
- using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+
 namespace PalTracker
 {
     public class Startup
@@ -31,6 +34,8 @@ namespace PalTracker
                             ,Configuration.GetValue<string>("CF_INSTANCE_INDEX","CF_INSTANCE_INDEX not found"),Configuration.GetValue<string>("CF_INSTANCE_ADDR","CF_INSTANCE_ADDR not found")));
             services.AddScoped<ITimeEntryRepository,MySqlTimeEntryRepository>();
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration)); 
+            services.AddCloudFoundryActuators(Configuration);
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }
